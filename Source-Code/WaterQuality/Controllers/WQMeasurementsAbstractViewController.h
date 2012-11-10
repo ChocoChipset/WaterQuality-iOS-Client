81997d41c0fa4787dc17eee1b0a1f243f7b24c83
Hector Zarate <hecktorzr@gmail.com>
2012-11-10 13:38:57 +0100

    Added WQMeasurementsAbstractViewController class

---
+++ Source-Code/WaterQuality/Controllers/WQMeasurementsAbstractViewController.h
@@ -0,0 +1,13 @@
+//
+//  WQMeasurementsAbstractViewController.h
+//  WaterQuality
+//
+//  Created by Hector Zarate on 11/10/12.
+//  Copyright (c) 2012 HackWAW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WQMeasurementsAbstractViewController : UIViewController
+
+@end
