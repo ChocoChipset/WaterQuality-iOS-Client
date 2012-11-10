7bb194911fb9b2808766a943c1852f3dc03c553b
Hector Zarate <hecktorzr@gmail.com>
2012-11-10 14:59:28 +0100

    Added Web Service Manager

---
+++ Source-Code/WaterQuality/Model/WQWebServiceManager.h
@@ -0,0 +1,17 @@
+//
+//  WQWebServiceManager.h
+//  WaterQuality
+//
+//  Created by Hector Zarate on 11/10/12.
+//  Copyright (c) 2012 HackWAW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WQWebServiceManager : NSObject
+
+
+
+
+@end
---
+++ Source-Code/WaterQuality/Model/WQWebServiceManager.m
@@ -0,0 +1,17 @@
+//
+//  WQWebServiceManager.m
+//  WaterQuality
+//
+//  Created by Hector Zarate on 11/10/12.
+//  Copyright (c) 2012 HackWAW. All rights reserved.
+//
+
+#import "WQWebServiceManager.h"
+#import "SVHTTPWQClient.h"  // SVHTTPWQClient should be used instead of SVHTTPClient
+
+
+@implementation WQWebServiceManager
+
+
+
+@end
