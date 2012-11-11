f5e61027aa5c56086222c26e982d072b2ad0ae33
Hector Zarate <hecktorzr@gmail.com>
2012-11-11 12:46:04 +0100

    Added Details Segue Key in Constants

--- Source-Code/WaterQuality/WQConstants.h
+++ Source-Code/WaterQuality/WQConstants.h
@@ -46,3 +46,9 @@
 #define kCODE_ICON_NAME_SUFFIX_LIST @"" // using same images as in main view right now
 #define kCODE_ICON_NAME_SUFFIX_MAP @"_map"
 #define kCODE_ICON_NAME_DEFAULT_INDEX 0
+
+
+
+// Segues Keys
+
+#define kSEGUE_PRESENT_MEASUREMENTS_DETAILS @"PresentMeasurementDetailsSegue"
